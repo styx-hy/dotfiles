ea971f0df227b48626c34f5a8ae419f61adf75e0
styx-hy <hy.styx@gmail.com>
2013-08-27 09:36:37 +0800

    fix echo to remove a newline on linux

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -64,6 +64,11 @@ rb_prompt() {
     echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
   else
     echo ""
+  #  version=$(rbenv version-name 2> /dev/null)
+  #  if [[ "$version" == "" ]] then version="-" fi
+  ##   echo "%{$fg_bold[yellow]%}$version%{$reset_color%}"
+  ## else
+  ##   echo ""
   fi
   echo "%{$fg_bold[yellow]%}%n%{$reset_color%}"
 }
