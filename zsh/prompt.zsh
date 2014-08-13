16dcee759fe7ac344d44a60af90898c9758d63e2
Yang Hong <hy.styx@gmail.com>
2014-08-13 15:54:36 +0000

    display full machine name

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -77,7 +77,7 @@ directory_name(){
 }
 
 host_name() {
-  echo "%{$fg_bold[magenta]%}%m%{$reset_color%}"
+  echo "%{$fg_bold[magenta]%}%M%{$reset_color%}"
 }
 
 export PROMPT=$'\n$(rb_prompt) at $(host_name) in $(directory_name) $(git_dirty)$(need_push)\n› '
