8a5d739ba10f8231f7f9f63bcc96d542c67f1e21
Yang Hong <yanghong@yanghong.org>
2015-08-12 14:49:55 +0800

    20150812
    fix ld path for local::lib

--- perl/local-lib.zsh
+++ perl/local-lib.zsh
@@ -1,4 +1,8 @@
 # FIXME the PATH variable should use push method
-[ $SHLVL -eq 1 ] && eval "$(perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=~/.perl5)"
+# This forces initialization only at the top level, e.g. the shell that
+# fires up tmux
+[ $SHLVL -eq 1 ] && eval "$(perl -I${HOME}/.perl5/lib/perl5 -Mlocal::lib=${HOME}/.perl5)"
+
 # FIXME why?!
 pushpath "/home/styx/.perl5/bin"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PERL_LOCAL_LIB_ROOT}/lib
--- system/path.zsh
+++ system/path.zsh
@@ -7,8 +7,3 @@ appendpath "$HOME/zion/projects/ssearcher"
 appendpath "$HOME/zion/racket/racket/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-if [[ -d "$HOME/.pb" ]]; then
-    export PERLBREW_ROOT=~/.pb
-    source ~/.pb/etc/bashrc
-fi
