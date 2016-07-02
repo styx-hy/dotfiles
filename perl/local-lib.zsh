01c05045ca9d99e5ba5983c674c5d6d422839ee8
Yang Hong <yanghong@yanghong.org>
2016-07-02 17:00:57 +0800

    disable local::lib

--- perl/local-lib.zsh
+++ perl/local-lib.zsh
@@ -1,8 +1,8 @@
 # FIXME the PATH variable should use push method
 # This forces initialization only at the top level, e.g. the shell that
 # fires up tmux
-[ $SHLVL -eq 1 ] && eval "$(perl -I${HOME}/.perl5/lib/perl5 -Mlocal::lib=${HOME}/.perl5)"
+# [ $SHLVL -eq 1 ] && eval "$(perl -I${HOME}/.perl5/lib/perl5 -Mlocal::lib=${HOME}/.perl5)"
 
 # FIXME why?!
-pushpath "/home/styx/.perl5/bin"
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PERL_LOCAL_LIB_ROOT}/lib
+# pushpath "/home/styx/.perl5/bin"
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PERL_LOCAL_LIB_ROOT}/lib
